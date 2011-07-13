688c63da01a9fef7071ad6ee3a181000b26a5166
Dmitry Lihachev <lda@openteam.ru>
2011-07-13 17:20:28 +0700

    generate models works

---
+++ lib/generators/ckeditor/templates/models/mongoid/attachment_file.rb
@@ -0,0 +1,15 @@
+class Ckeditor::AttachmentFile < Ckeditor::Asset
+  has_attached_file :data,
+                    :url => "/ckeditor_assets/attachments/:id/:filename",
+                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
+  
+  validates_attachment_size :data, :less_than => 100.megabytes
+  validates_attachment_presence :data
+	
+	def url_thumb
+	  @url_thumb ||= begin
+	    extname = File.extname(filename).gsub(/^\./, '')
+      "/javascripts/ckeditor/filebrowser/images/thumbs/#{extname}.gif"
+	  end
+	end
+end
--- test/generators/models_generator_test.rb
+++ test/generators/models_generator_test.rb
@@ -7,15 +7,23 @@ class ModelsGeneratorTest < Rails::Generators::TestCase
 
   test "models and migration for active_record orm" do
     run_generator %w(--orm=active_record)
-    
+
     assert_file "app/models/ckeditor/asset.rb"
     assert_file "app/models/ckeditor/picture.rb"
     assert_file "app/models/ckeditor/attachment_file.rb"
-    
+
     assert_migration "db/migrate/create_ckeditor_assets.rb" do |migration|
       assert_class_method :up, migration do |up|
         assert_match /create_table/, up
       end
     end
+  end
+
+  test "models and migration for mongoid orm" do
+    run_generator %w(--orm=mongoid)
+
+    assert_file "app/models/ckeditor/asset.rb"
+    assert_file "app/models/ckeditor/picture.rb"
+    assert_file "app/models/ckeditor/attachment_file.rb"
   end
 end
