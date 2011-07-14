e6204e83b67235684a2d371a6ef9890186d82e43
Dmitry Lihachev <lda@openteam.ru>
2011-07-14 16:18:31 +0700

    prepare to support mongoid tests

--- lib/generators/ckeditor/templates/models/mongoid/asset.rb
+++
@@ -1,7 +0,0 @@
-require 'mime/types'
-
-class Ckeditor::Asset < ActiveRecord::Base
-  include Ckeditor::Orm::ActiveRecord::AssetBase
-  
-  attr_accessible :data, :assetable_type, :assetable_id, :assetable
-end
--- test/orm/active_record.rb
+++ test/orm/active_record.rb
@@ -1,9 +1,1 @@
-#ActiveRecord::Migration.verbose = false
-#ActiveRecord::Base.logger = Logger.new(nil)
-
 ActiveRecord::Migrator.migrate(File.expand_path("../../dummy/db/migrate/", __FILE__))
-
-#class ActiveSupport::TestCase
-  #self.use_transactional_fixtures = true
-  #self.use_instantiated_fixtures  = false
-#end
