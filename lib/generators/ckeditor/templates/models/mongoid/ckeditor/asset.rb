a00f086c5d9a48dcdef3c5cc2d1eb3acc9906759
Dmitry Lihachev <lda@openteam.ru>
2011-07-15 17:55:59 +0700

    all tests passes

--- lib/generators/ckeditor/templates/models/mongoid/ckeditor/asset.rb
+++ lib/generators/ckeditor/templates/models/mongoid/ckeditor/asset.rb
@@ -1,7 +1,7 @@
 require 'mime/types'
 
-class Ckeditor::Asset < ActiveRecord::Base
-  include Ckeditor::Orm::ActiveRecord::AssetBase
-  
+class Ckeditor::Asset
+  include Ckeditor::Orm::Mongoid::AssetBase
+
   attr_accessible :data, :assetable_type, :assetable_id, :assetable
 end
