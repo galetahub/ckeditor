a63737f8915c4a29ac86f749b3a07fb42e75abc8
Tomasz Orzechowski <tomasz.orzechowski@netmedia.com.pl>
2011-09-02 11:52:46 +0200

    Do not enlarge images smaller than 800px

--- lib/generators/ckeditor/templates/models/base/carrierwave/uploaders/ckeditor_picture_uploader.rb
+++ lib/generators/ckeditor/templates/models/base/carrierwave/uploaders/ckeditor_picture_uploader.rb
@@ -36,7 +36,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   end
 
   version :content do
-    process :resize_to_fit => [800, 800]
+    process :resize_to_limit => [800, 800]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
