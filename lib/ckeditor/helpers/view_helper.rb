ddb417d501e8244a9e97b21de188d2cadadd869f
Ace Suares <ace@suares.an>
2011-10-03 08:01:51 -0400

    * damn hash

--- lib/ckeditor/helpers/view_helper.rb
+++ lib/ckeditor/helpers/view_helper.rb
@@ -6,7 +6,7 @@ module Ckeditor
       def cktext_area_tag(name, content = nil, options = {})
         input_html = options.delete(:input_html) || {}
         element_id = input_html.delete(:id) || sanitize_to_id(name)
-        input_html << { :id => element_id }
+        input_html[:id]= element_id
 
         options = { :language => I18n.locale.to_s }.merge(options)
 
