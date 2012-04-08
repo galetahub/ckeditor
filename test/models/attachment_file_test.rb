ccc4d64f1f34fb2a56410fbaf9271140e2e12cbe
James Le Cuirot <chewi@aura-online.co.uk>
2012-04-08 14:09:43 +0100

    Fix some typos.

--- test/models/attachment_file_test.rb
+++ test/models/attachment_file_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class AttachmentFileTest < ActiveSupport::TestCase
   test "Set file content_type and size" do
-    @attachemnt = create_attachemnt
+    @attachment = create_attachment
     
     # TODO: fix filename parameterization
     if CKEDITOR_BACKEND == :paperclip
-      assert_equal @attachemnt.data_file_name, "rails_tar.gz"
+      assert_equal @attachment.data_file_name, "rails_tar.gz"
     else
-      assert_equal @attachemnt.data_file_name, "rails.tar.gz"
+      assert_equal @attachment.data_file_name, "rails.tar.gz"
     end
     
-    assert_equal @attachemnt.data_content_type, "application/x-gzip"
-    assert_equal @attachemnt.data_file_size, 6823
-    assert_equal @attachemnt.url_thumb, "/assets/ckeditor/filebrowser/images/thumbs/gz.gif"
+    assert_equal @attachment.data_content_type, "application/x-gzip"
+    assert_equal @attachment.data_file_size, 6823
+    assert_equal @attachment.url_thumb, "/assets/ckeditor/filebrowser/images/thumbs/gz.gif"
   end
 end
