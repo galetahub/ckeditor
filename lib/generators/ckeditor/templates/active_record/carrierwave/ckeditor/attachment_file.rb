74f3460c0b3c5d34c05b4e4281adbbcea4ead137
Igor Galeta <galeta.igor@gmail.com>
2012-08-23 12:06:29 +0300

    Replace tabs

--- lib/generators/ckeditor/templates/active_record/carrierwave/ckeditor/attachment_file.rb
+++ lib/generators/ckeditor/templates/active_record/carrierwave/ckeditor/attachment_file.rb
@@ -1,7 +1,7 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   mount_uploader :data, CkeditorAttachmentFileUploader, :mount_on => :data_file_name
-	
-	def url_thumb
-	  @url_thumb ||= Ckeditor::Utils.filethumb(filename)
-	end
+
+  def url_thumb
+    @url_thumb ||= Ckeditor::Utils.filethumb(filename)
+  end
 end
--- lib/generators/ckeditor/templates/active_record/paperclip/migration.rb
+++ lib/generators/ckeditor/templates/active_record/paperclip/migration.rb
@@ -6,18 +6,18 @@ class CreateCkeditorAssets < ActiveRecord::Migration
       t.integer :data_file_size
       
       t.integer :assetable_id
-		  t.string  :assetable_type, :limit => 30
+      t.string  :assetable_type, :limit => 30
       t.string  :type, :limit => 30
-		  
+
       # Uncomment	it to save images dimensions, if your need it
-		  t.integer :width
-		  t.integer :height
-		  
+      t.integer :width
+      t.integer :height
+
       t.timestamps
     end
     
     add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
-		add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
+    add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
   end
 
   def self.down
