49ac6a90b8397478d8f2baa2757fcd539a585da5
Igor Galeta <galeta.igor@gmail.com>
2012-08-23 13:52:46 +0300

    Change default uploads path

--- lib/generators/ckeditor/templates/base/carrierwave/uploaders/ckeditor_picture_uploader.rb
+++ lib/generators/ckeditor/templates/base/carrierwave/uploaders/ckeditor_picture_uploader.rb
@@ -13,7 +13,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "system/ckeditor_assets/pictures/#{model.id}"
+    "/uploads/ckeditor/pictures/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- lib/generators/ckeditor/templates/base/dragonfly/initializer.rb
+++ lib/generators/ckeditor/templates/base/dragonfly/initializer.rb
@@ -11,15 +11,15 @@ app.define_macro(ActiveRecord::Base, :ckeditor_file_accessor) if defined?(Active
 app.define_macro_on_include(Mongoid::Document, :ckeditor_file_accessor) if defined?(Mongoid::Document)
 
 app.configure do |c|
-  # Store files in public/system/ckeditor_assets. This is not
+  # Store files in public/uploads/ckeditor. This is not
   # mandatory and the files don't even have to be stored under
   # public. If not storing under public then set server_root to nil.
-  c.datastore.root_path = Rails.root.join("public", "system", "ckeditor_assets", Rails.env).to_s
+  c.datastore.root_path = Rails.root.join("public", "uploads", "ckeditor", Rails.env).to_s
   c.datastore.server_root = Rails.root.join("public").to_s
 
   # Accept asset requests on /ckeditor_assets. Again, this is not
   # mandatory. Just be sure to include :job somewhere.
-  c.url_format = "/ckeditor_assets/:job/:basename.:format"
+  c.url_format = "/uploads/ckeditor/:job/:basename.:format"
 end
 
 # Insert our Dragonfly "app" into the stack.
