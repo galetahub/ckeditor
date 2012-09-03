1f283c35821dcad3598b3e630d639b78150f3a0e
Igor Galeta <galeta.igor@gmail.com>
2012-09-03 14:00:36 +0300

    Fix carrierwave upload path

--- lib/generators/ckeditor/templates/base/carrierwave/uploaders/ckeditor_attachment_file_uploader.rb
+++ lib/generators/ckeditor/templates/base/carrierwave/uploaders/ckeditor_attachment_file_uploader.rb
@@ -13,7 +13,7 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/uploads/ckeditor/attachments/#{model.id}"
+    "uploads/ckeditor/attachments/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
