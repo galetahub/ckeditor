38ab8e3c7e04f98666fe46a8e43536f4bae6eebb
Igor Galeta <galeta.igor@gmail.com>
2013-01-05 14:17:11 +0200

    Remove my ckeditor plugins

--- app/assets/javascripts/ckeditor/plugins/embed/lang/ru.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.plugins.setLang('embed', 'ru',
-{
-  embed : 
-  {
-    title : "Вставить embed",
-    button : "Вставить embed",
-    pasteMsg : "Пожалуйста, вставьте embed-код с Youtube, Myspace, Flickr и других ресурсов в прямоугольник, используя сочетание клавиш (Ctrl+V), и нажмите OK."
-  }
-});
