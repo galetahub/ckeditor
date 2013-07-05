5fb93b3a13b24b41b57ff26b05af2053d310109f
Igor Galeta <galeta.igor@gmail.com>
2013-07-05 16:28:58 +0300

    Check if assets pipeline enabled

---
+++ app/helpers/ckeditor/application_helper.rb
@@ -0,0 +1,11 @@
+class Ckeditor::ApplicationHelper
+  def assets_pipeline_enabled?
+    if Gem::Version.new(::Rails.version.to_s) >= Gem::Version.new('4.0.0')
+      defined?(Sprockets::Rails)
+    elsif Gem::Version.new(::Rails.version.to_s) >= Gem::Version.new('3.0.0')
+      Rails.application.config.assets.enabled
+    else
+      false
+    end
+  end
+end
