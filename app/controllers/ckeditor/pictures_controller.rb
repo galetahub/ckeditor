bea8e56520cbc6329b97427f2b4e95cf22fa759b
Mathieu Gagné <mathieu@motioneleven.com>
2013-09-05 10:45:02 -0400

    Add pagination with Kaminari to pictures#index

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -2,21 +2,22 @@ class Ckeditor::PicturesController < Ckeditor::ApplicationController
 
   def index
     @pictures = Ckeditor.picture_model.find_all(ckeditor_pictures_scope)
-    respond_with(@pictures) 
+    @pictures = Kaminari.paginate_array(@pictures).page(params[:page]).per(20)
+    respond_with(@pictures)
   end
-  
+
   def create
     @picture = Ckeditor::Picture.new
 	  respond_with_asset(@picture)
   end
-  
+
   def destroy
     @picture.destroy
     respond_with(@picture, :location => pictures_path)
   end
-  
+
   protected
-  
+
     def find_asset
       @picture = Ckeditor.picture_model.get!(params[:id])
     end
