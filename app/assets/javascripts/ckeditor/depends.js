597d59a666cd44b4f0593f4f46559e1dee41dca3
Scott Nelson <scott@nelson.name>
2014-02-24 09:19:36 -0500

    Add depends_on_asset and depends_on directives to recompile override.js when related javascripts are changed.

---
+++ app/assets/javascripts/ckeditor/depends.js
@@ -0,0 +1,5 @@
+/*
+    Include any additional depends_on_asset or depends_on directives here.
+    Include all files for any additional plugins you may have installed.
+    If any of these are changed, then ckeditor/override.js will be recompiled to get the proper compiled asset path.
+*/
