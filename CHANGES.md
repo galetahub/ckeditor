37cf36f607dc25bb6685f9ef6b7e797139bccc4d
Igor Galeta <galeta.igor@gmail.com>
2014-07-14 10:33:33 +0300

    Update ckeditor js to 4.4.2 full version

---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+# Ckeditor changelog
+
+## Ckeditor 4.1.0
+  
+  * Update ckeditor to 4.4.2 Full Package version (72 Plugins, 24 Jun 2014)
+  * Starting gem changelog
--- lib/ckeditor/version.rb
+++ lib/ckeditor/version.rb
@@ -1,6 +1,6 @@
 module Ckeditor
   module Version
-    GEM = "4.0.11".freeze
-    EDITOR = "4.3.4".freeze
+    GEM = "4.1.0".freeze
+    EDITOR = "4.4.2".freeze
   end
 end
