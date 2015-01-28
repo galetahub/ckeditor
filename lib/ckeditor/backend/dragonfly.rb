88718c1884416ffc7c662fe65898bab3b03b1e66
Thiago A. Silva <thiagoaraujos@gmail.com>
2015-01-28 17:53:42 -0300

    Update Dragonfly backend and generators to work with Dragonfly 1.0

--- lib/ckeditor/backend/dragonfly.rb
+++ lib/ckeditor/backend/dragonfly.rb
@@ -2,24 +2,26 @@ module Ckeditor
   module Backend
     module Dragonfly
       def self.included(base)
-        base.send(:include, InstanceMethods)
+        base.send(:extend, ::Dragonfly::Model)
+        base.send(:extend, ::Dragonfly::Model::Validations)
         base.send(:extend, ClassMethods)
+        base.send(:include, InstanceMethods)
       end
 
       module ClassMethods
         def attachment_file_types
-          @attachment_file_types ||= Ckeditor.attachment_file_types.map(&:to_sym).tap do |formats|
+          @attachment_file_types ||= Ckeditor.attachment_file_types.map(&:to_s).tap do |formats|
             # This is not ideal but Dragonfly doesn't return double
             # extensions. Having said that, the other backends
             # currently don't use attachment_file_types at all.
-            [ :bz2, :gz, :lzma, :xz ].each do |f|
-              formats << f if formats.include?("tar.#{f}".to_sym)
+            [ 'bz2', 'gz', 'lzma', 'xz' ].each do |f|
+              formats << f if formats.include?("tar.#{f}")
             end
           end
         end
 
         def image_file_types
-          @image_file_types ||= Ckeditor.image_file_types.map(&:to_sym)
+          @image_file_types ||= Ckeditor.image_file_types.map(&:to_s)
         end
       end
 
@@ -27,7 +29,7 @@ module Ckeditor
         delegate :url, :path, :size, :image?, :width, :height, :to => :data
 
         alias_attribute :data_file_name, :data_name
-        alias_attribute :data_content_type, :data_mime_type
+        alias_attribute :data_content_type, :"data.mime_type"
         alias_attribute :data_file_size, :data_size
 
         private
--- lib/generators/ckeditor/templates/base/dragonfly/initializer.rb
+++ lib/generators/ckeditor/templates/base/dragonfly/initializer.rb
@@ -1,26 +1,20 @@
-# Load Dragonfly for Rails if it isn't loaded already.
-require "dragonfly/rails/images"
+# Load Dragonfly if it isn't loaded already.
+require "dragonfly"
 
-# Use a separate Dragonfly "app" for CKEditor.
-app = Dragonfly[:ckeditor]
-app.configure_with(:rails)
-app.configure_with(:imagemagick)
+Dragonfly.app(:ckeditor).configure do
+  plugin :imagemagick
+  secret "<%= SecureRandom.hex(32) %>"
 
-# Define the ckeditor_file_accessor macro.
-app.define_macro(ActiveRecord::Base, :ckeditor_file_accessor) if defined?(ActiveRecord::Base)
-app.define_macro_on_include(Mongoid::Document, :ckeditor_file_accessor) if defined?(Mongoid::Document)
-
-app.configure do |c|
   # Store files in public/uploads/ckeditor. This is not
   # mandatory and the files don't even have to be stored under
-  # public. If not storing under public then set server_root to nil.
-  c.datastore.root_path = Rails.root.join("public", "uploads", "ckeditor", Rails.env).to_s
-  c.datastore.server_root = Rails.root.join("public").to_s
+  # public. See http://markevans.github.io/dragonfly/data-stores
+  datastore :file,
+    root_path: Rails.root.join("public", "uploads", "ckeditor", Rails.env).to_s,
+    server_root: 'public'
 
-  # Accept asset requests on /ckeditor_assets. Again, this is not
-  # mandatory. Just be sure to include :job somewhere.
-  c.url_format = "/uploads/ckeditor/:job/:basename.:format"
+  # Accept asset requests on /ckeditor_assets. Again, this path is
+  # not mandatory. Just be sure to include :job somewhere.
+  url_format "/uploads/ckeditor/:job/:basename.:format"
 end
 
-# Insert our Dragonfly "app" into the stack.
-Rails.application.middleware.insert_after Rack::Cache, Dragonfly::Middleware, :ckeditor
+Rails.application.middleware.use Dragonfly::Middleware, :ckeditor
