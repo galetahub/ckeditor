6cfbbdb67f951a34ff376c87595583bd468b13ff
CodedBeardedSignedTaylor <taylorjonessoftware@gmail.com>
2015-03-04 16:26:18 -0600

    Inserted wrong plugin

---
+++ vendor/assets/javascripts/ckeditor/plugins/codesnippet/lang/es.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.plugins.setLang( 'codesnippet', 'es', {
+	button: 'Insertar fragmento de código',
+	codeContents: 'Code content', // MISSING
+	emptySnippetError: 'A code snippet cannot be empty.', // MISSING
+	language: 'Language', // MISSING
+	title: 'Code snippet', // MISSING
+	pathName: 'code snippet' // MISSING
+} );
