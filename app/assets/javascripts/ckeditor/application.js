39db187f447597bcf61e854aa2772bb795ba7b9b
Igor Galeta <galeta.igor@gmail.com>
2016-01-04 21:48:28 +0200

    Load jquery from code.jquery.com for filebrowser

--- app/assets/javascripts/ckeditor/application.js
+++ app/assets/javascripts/ckeditor/application.js
@@ -1,4 +1,3 @@
-//= require ckeditor/filebrowser/javascripts/jquery.min.js
 //= require ckeditor/filebrowser/javascripts/jquery.tmpl.min.js
 //= require ckeditor/filebrowser/javascripts/fileuploader.js
 //= require ckeditor/filebrowser/javascripts/jquery.endless-scroll.js
--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -33,65 +33,6 @@ CKEDITOR.editorConfig = function( config )
 
   config.allowedContent = true;
 
-  // Rails CSRF token
-  config.filebrowserParams = function(){
-    var csrf_token, csrf_param, meta,
-        metas = document.getElementsByTagName('meta'),
-        params = new Object();
-
-    for ( var i = 0 ; i < metas.length ; i++ ){
-      meta = metas[i];
-
-      switch(meta.name) {
-        case "csrf-token":
-          csrf_token = meta.content;
-          break;
-        case "csrf-param":
-          csrf_param = meta.content;
-          break;
-        default:
-          continue;
-      }
-    }
-
-    if (csrf_param !== undefined && csrf_token !== undefined) {
-      params[csrf_param] = csrf_token;
-    }
-
-    return params;
-  };
-
-  config.addQueryString = function( url, params ){
-    var queryString = [];
-
-    if ( !params ) {
-      return url;
-    } else {
-      for ( var i in params )
-        queryString.push( i + "=" + encodeURIComponent( params[ i ] ) );
-    }
-
-    return url + ( ( url.indexOf( "?" ) != -1 ) ? "&" : "?" ) + queryString.join( "&" );
-  };
-
-  // Integrate Rails CSRF token into file upload dialogs (link, image, attachment and flash)
-  CKEDITOR.on( 'dialogDefinition', function( ev ){
-    // Take the dialog name and its definition from the event data.
-    var dialogName = ev.data.name;
-    var dialogDefinition = ev.data.definition;
-    var content, upload;
-
-    if (CKEDITOR.tools.indexOf(['link', 'image', 'attachment', 'flash'], dialogName) > -1) {
-      content = (dialogDefinition.getContents('Upload') || dialogDefinition.getContents('upload'));
-      upload = (content == null ? null : content.get('upload'));
-
-      if (upload && upload.filebrowser && upload.filebrowser['params'] === undefined) {
-        upload.filebrowser['params'] = config.filebrowserParams();
-        upload.action = config.addQueryString(upload.action, upload.filebrowser['params']);
-      }
-    }
-  });
-
   // Toolbar groups configuration.
   config.toolbar = [
     { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source'] },
