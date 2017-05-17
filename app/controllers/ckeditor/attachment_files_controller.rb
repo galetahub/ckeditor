e812fc857e30ee45cf76cad8999891858023e07c
pmoore <pmoore@weddingwire.com>
2017-05-17 13:16:26 -0400

    Updating skip_before_action to not raise error if method is not already in filter chain.
    In this case, it's when the host app's ApplicationController is not performing protect_from_forgery which adds verify_authenticity_token in the filter chain.

--- app/controllers/ckeditor/attachment_files_controller.rb
+++ app/controllers/ckeditor/attachment_files_controller.rb
@@ -1,5 +1,5 @@
 class Ckeditor::AttachmentFilesController < Ckeditor::ApplicationController
-  skip_before_action :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token, only: :create, raise: false
 
   def index
     @attachments = Ckeditor.attachment_file_adapter.find_all(ckeditor_attachment_files_scope)
