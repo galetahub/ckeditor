4e4472bbcb7629b632de0d2de80b623a7bf34a50
Igor Galeta <galeta.igor@gmail.com>
2017-06-06 12:17:53 +0300

    Update test: now paperclick don't parameterize filename

--- test/models/attachment_file_test.rb
+++ test/models/attachment_file_test.rb
@@ -5,15 +5,10 @@ class AttachmentFileTest < ActiveSupport::TestCase
     @attachment.destroy rescue nil
   end
 
-  test "Set file content_type and size" do
+  test 'Set file content_type and size' do
     @attachment = create_attachment
 
-    # TODO: fix filename parameterization
-    if CKEDITOR_BACKEND == :paperclip
-      assert_equal "rails_tar.gz", @attachment.data_file_name
-    else
-      assert_equal "rails.tar.gz", @attachment.data_file_name
-    end
+    assert_equal "rails.tar.gz", @attachment.data_file_name
 
     assert_match /application\/(x-)?gzip/, @attachment.data_content_type
     assert_equal 6823, @attachment.data_file_size
